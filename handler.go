6f8d473f5a59c728b611f1704aeb81a07e29c71d
erikathea <ethajes@gmail.com>
2024-06-06 19:20:27 -0300

    test JSON response

--- handler.go
+++ handler.go
@@ -1,19 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 )
 
+type Response struct {
+	Text string `json:"text"`
+}
+
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	message := "This HTTP triggered function executed successfully"
 	name := r.URL.Query().Get("name")
 	if name != "" {
-		message = fmt.Sprintf("Hello, %s. \n TEST OK", name)
+		message = fmt.Sprintf("Hello, %s", name)
 	}
-	fmt.Fprint(w, message)
+
+	response := Response{Text: message}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(response)
 }
 
 func main() {
